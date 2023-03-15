d33b6e8eb315ef1d4f8e0b112b119663d70cbf04
Pablo <pablobhz@gmail.com>
2023-03-15 10:43:23 -0300

    Initial commit

---
+++ springmail/src/test/java/com/ms/springmail/SpringmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.springmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
